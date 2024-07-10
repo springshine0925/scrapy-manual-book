fbb1ca9a477d3290c14b7603fbdf9152a233d0b4
springshine0925 <wilsonchan020@gmail.com>
2024-07-10 14:50:53 +0800

    r-go-tools result

--- scrapy_spider/spiders/Rgotools.py
+++ scrapy_spider/spiders/Rgotools.py
@@ -7,28 +7,35 @@ class BoompodsSpider(SitemapSpider):
     sitemap_urls = ['https://www.r-go-tools.nl/product-sitemap1.xml']
 
     def parse(self, response, **cb_kwargs):
-        for doc in response.css('a[href$=".pdf"]'):
-            product = response.css('.posted_in a::text').get()
-            model = response.css('h1.product_title.entry-title::text').re_first(r'(?i)(?:R-Go)?\s*(.+)')
-            words = model.split()
-            if len(words) >= 2:
-                re_model = " ".join(words[:2])
-            else:
-                re_model = model
-            doc_type = "Manual"
-            language = response.css('::attr("lang")').get()[:2]
-            gtin = response.css('p.variation-gtin::text').get()
-            eans = re.search(r'\d+', gtin).group()
-            
-            yield Manual(
-                model=re_model,
-                brand='R-Go Tools',
-                product=product,
-                product_lang= 'en',
-                file_urls=[doc.css('::attr("href")').get().strip()],
-                type=doc_type,
-                eans = eans,
-                url=response.url,
-                thumb_urls=response.css('img.wp-post-image.wvg-post-image.attachment-woocommerce_single.size-woocommerce_single ::attr("srcset")').get().split()[-2],
-                source=self.name,
-            )
+        # yield{'url': response.url}
+
+        for doc in response.css('ul.links li.download:first-child a[href$=".pdf"]'):
+            manual_link = response.css('ul.links li.download:first-child a')
+            manual_text = manual_link.css('::text').get()
+            manual_url = manual_link.css('::attr(href)').get()
+            if "Handleiding" in manual_text and manual_url:
+                product = response.css('.posted_in a::text').get()
+                model = response.css('h1.product_title.entry-title::text').re_first(r'(?i)(?:R-Go)?\s*(.+)')
+                words = model.split()
+                if len(words) >= 2:
+                    re_model = " ".join(words[:2])
+                else:
+                    re_model = model
+                # print(re_model)
+                doc_type = "Manual"
+                language = response.css('::attr("lang")').get()[:2]
+                gtin = response.css('p.variation-gtin::text').get()
+                eans = re.search(r'\d+', gtin).group()
+                pdf_url= doc.css('a::attr("href")').get().strip()
+                yield Manual(
+                    model=re_model,
+                    brand='R-Go Tools',
+                    product=product,
+                    product_lang= 'en',
+                    file_urls=[pdf_url],
+                    type=doc_type,
+                    eans = eans,
+                    url=response.url,
+                    thumb_urls=response.css('img.wp-post-image.wvg-post-image.attachment-woocommerce_single.size-woocommerce_single ::attr("srcset")').get().split()[-2],
+                    source=self.name,
+                )
